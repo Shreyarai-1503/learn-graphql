96eaa918f4bae03e4bcdc82313e7a6adffec1638
Shreya <shreyarai1503@gmail.com>
2024-10-23 11:21:46 +0530

    set-up graphQL server

--- threads-backend/src/index.ts
+++ threads-backend/src/index.ts
@@ -1,14 +1,28 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
 
-const app = express();
-const PORT = Number(process.env.PORT) || 8000;
+async function init() {
+    const app = express();
+    const PORT = Number(process.env.PORT) || 8000;
+
+    //Create GraphQL server
+    const gqlServer = new ApolloServer({
+        typeDefs: '',
+        resolvers: {}
+    });
+
+    //Start GraphQL the server
+    await gqlServer.start();
+
+    app.get('/', (req, res) => {
+        res.json({ message: 'Server is up and running' });
+    });
 
 
-app.get('/', (req, res) => {
-    res.json({message: 'Server is up and running'});
-})
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+init();
