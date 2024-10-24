a82f18409c86dfff8879474ba2c6cf24776b40e2
Shreya <shreyarai1503@gmail.com>
2024-10-24 11:00:45 +0530

    code refactor

---
+++ threads-backend/src/graphql/index.ts
@@ -0,0 +1,31 @@
+import { ApolloServer } from '@apollo/server';
+import {User} from './user';
+
+async function createApolloGraphqlServer() {
+    //Create GraphQL server 
+    const gqlServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                ${User.queries}
+            }
+            type Mutation {
+                ${User.mutations}
+            }
+        `,  //Schema goes here
+        resolvers: {
+            Query: {
+                ...User.resolvers.queries
+            },
+            Mutation: {
+                ...User.resolvers.mutations
+            }    //Functions to resolve queries and mutations
+        }
+    })
+
+    //Start GraphQL the server
+    await gqlServer.start();
+
+    return gqlServer;
+}
+
+export default createApolloGraphqlServer;
--- threads-backend/src/index.ts
+++ threads-backend/src/index.ts
@@ -1,55 +1,18 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
-import { prismaClient } from './lib/db';
+import { expressMiddleware } from '@apollo/server/express4';
+import createApolloGraphqlServer from './graphql';
 
 async function init() {
     const app = express();
     const PORT = Number(process.env.PORT) || 8000;
 
-    //Create GraphQL server 
-    const gqlServer = new ApolloServer({
-        typeDefs: `
-            type Query {
-                hello: String
-                say(name: String): String
-            }
-            type Mutation {
-                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
-            }
-        `,                                              //Schema goes here
-        resolvers: {
-            Query: {
-                hello: () => 'Hello World',
-                say: (_, { name }: { name: string }) => `Hello ${name}`
-            },
-            Mutation: {
-                createUser: async (_, { firstName, lastName, email, password }:
-                    { firstName: string, lastName: string, email: string, password: string }) => {
-                    await prismaClient.user.create({
-                        data: {
-                            firstName,
-                            lastName,
-                            email,
-                            password,
-                            salt: '1234'
-                        }
-                    });
-                    return true;
-                }
-            }                                               //Functions to resolve queries and mutations
-        }
-    })
-
     app.use(express.json());
-
-    //Start GraphQL the server
-    await gqlServer.start();
 
     app.get('/', (req, res) => {
         res.json({ message: 'Server is up and running' });
     });
 
+    const gqlServer = await createApolloGraphqlServer();
     //Expose the GraphQL server
     app.use('/graphql', expressMiddleware(gqlServer));
 
