43976c02944a82f82f4fa196c8c7d44f36f894b0
Shreya <shreyarai1503@gmail.com>
2024-10-27 20:49:07 +0530

    create user service

---
+++ threads-backend/src/services/user.ts
@@ -0,0 +1,30 @@
+import { createHmac, randomBytes } from "node:crypto"
+import { prismaClient } from "../lib/db";
+
+export interface CreateUserPayload {
+    firstName: string
+    lastName?: string
+    email: string
+    password: string
+}
+
+class UserService {
+    public static createUser(payload: CreateUserPayload) {
+        const { firstName, lastName, email, password } = payload
+        const salt = randomBytes(16).toString();
+        const hashedPassword = createHmac("sha256", salt).update(password).digest("hex");
+
+        return prismaClient.user.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+                salt
+            }
+        })
+
+    }
+}
+
+export default UserService;
