aac1f6a01ab8603f09e5dd326224cb584c78b062
Shreya <shreyarai1503@gmail.com>
2024-10-27 21:29:51 +0530

    modify the resolver for createUser

--- threads-backend/src/graphql/user/resolvers.ts
+++ threads-backend/src/graphql/user/resolvers.ts
@@ -1,8 +1,11 @@
+import UserService, { CreateUserPayload } from "../../services/user";
+
 const queries = {}
 
 const mutations = {
-    createUser: async (_: any, { }: {}) => {
-        return 'randomId'
+    createUser: async (_: any, payload: CreateUserPayload) => {
+        const res = await UserService.createUser(payload); 
+        return res.id;
     }
 }
 
